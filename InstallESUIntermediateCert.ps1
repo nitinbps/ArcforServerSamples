80a30ee07c2d79cbde169d6451424edaab9f3e87
Nitin Gupta <nitingup@ntdev.microsoft.com>
2023-11-20 11:53:09 -0800

    Adding intermediate cert utility

---
+++ InstallESUIntermediateCert.ps1
@@ -0,0 +1,29 @@
+﻿
+function Install-ESUIntermediateCert 
+{
+    [CmdletBinding()]
+    param ()
+
+    # run this script on elevated powershell.exe 
+    $esuTempFolder = "$env:windir\esuTemp"
+    $esutempDirectoryExisted = $false
+    if( -not (Test-Path $esuTempFolder)) {
+        New-Item -Path $esuTempFolder -ItemType Directory -Force
+        $esutempDirectoryExisted = $true
+     }
+
+    Push-Location $esuTempFolder 
+ 
+    Invoke-WebRequest -Uri "https://www.microsoft.com/pkiops/certs/Microsoft%20Azure%20TLS%20Issuing%20CA%2001%20-%20xsign.crt" -OutFile ./matlsissuingca01.crt
+    certutil -addstore CA ./matlsissuingca01.crt
+ 
+    Pop-Location
+
+    #Remove the temp directory
+    if( -not $esutempDirectoryExisted){
+        Remove-Item -Recurse -Path $esuTempFolder -force -verbose
+    }
+}
+
+Install-ESUIntermediateCert  -verBose
+##Reboot the machine if machine needs a reboot before installing the updates.
