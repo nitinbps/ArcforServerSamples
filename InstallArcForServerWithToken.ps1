cc9062971037aa9fa36b5197a862b9a66429c66b
Nitin Gupta <nitingup@ntdev.microsoft.com>
2024-11-04 23:18:51 -0800

    Adding logic to fetch tenantId from machine:

--- InstallArcForServerWithToken.ps1
+++ InstallArcForServerWithToken.ps1
@@ -6,6 +6,40 @@ param(
 
 $global:scriptPath = $myinvocation.mycommand.definition
 
+function Get-TenantId {
+ 
+    [cmdletbinding()]
+    param([Parameter(Mandatory=$true)][string]$token)
+ 
+    #Validate as per https://tools.ietf.org/html/rfc7519
+    #Access and ID tokens are fine, Refresh tokens will not work
+    if (!$token.Contains(".") -or !$token.StartsWith("eyJ")) { Write-Error "Invalid token" -ErrorAction Stop }
+ 
+    #Header
+    $tokenheader = $token.Split(".")[0].Replace('-', '+').Replace('_', '/')
+    #Fix padding as needed, keep adding "=" until string length modulus 4 reaches 0
+    while ($tokenheader.Length % 4) { Write-Verbose "Invalid length for a Base-64 char array or string, adding ="; $tokenheader += "=" }
+   
+    #Convert from Base64 encoded string to PSObject all at once
+    Write-Verbose "Decoded header:"
+    [System.Text.Encoding]::ASCII.GetString([system.convert]::FromBase64String($tokenheader)) | ConvertFrom-Json | fl | Out-Default
+ 
+    #Payload
+    $tokenPayload = $token.Split(".")[1].Replace('-', '+').Replace('_', '/')
+    #Fix padding as needed, keep adding "=" until string length modulus 4 reaches 0
+    while ($tokenPayload.Length % 4) { Write-Verbose "Invalid length for a Base-64 char array or string, adding ="; $tokenPayload += "=" }
+
+    #Convert to Byte array
+    $tokenByteArray = [System.Convert]::FromBase64String($tokenPayload)
+    #Convert to string array
+    $tokenArray = [System.Text.Encoding]::ASCII.GetString($tokenByteArray)
+
+    #Convert from JSON to PSObject
+    $tokobj = $tokenArray | ConvertFrom-Json
+    
+    return $tokobj.tid
+}
+
 function Get-MachineDetails {
     param(
           [Parameter(Mandatory)] # Don't add Mandatory if it's false. Cause this the default
@@ -15,6 +49,7 @@ function Get-MachineDetails {
     # get token
     $content=Invoke-WebRequest -UseBasicParsing -Method Get -Uri "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&object_id=$princiPalId&resource=$env:AZUREURI" -Headers @{Metadata="true"}
     $access_token = ($content.Content|ConvertFrom-Json).access_token
+    $tenantID = Get-TenantId -token $access_token
 
     # machine details
     $content=Invoke-WebRequest -UseBasicParsing -Method Get -Uri "http://169.254.169.254/metadata/instance?api-version=2021-02-01" -Headers @{Metadata="true"} 
@@ -29,6 +64,7 @@ function Get-MachineDetails {
         'subscriptionID' = $subscriptionID
         'resourceGroupName' = $resourceGroupName
         'resourceLocation' = $resourceLocation
+        'tenantID' = $tenantID
 
     }
 
@@ -103,6 +139,7 @@ try {
     $env:SUBSCRIPTION_ID = $machine_info.subscriptionID;
     $env:RESOURCE_GROUP = $machine_info.resourceGroupName;
     $env:LOCATION = $machine_info.resourceLocation;
+    $env:TENANT_ID = $machine_info.tenantID;
     
 
     # Download the installation package
